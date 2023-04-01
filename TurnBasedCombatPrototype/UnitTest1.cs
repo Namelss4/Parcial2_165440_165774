f3946b30fb9e2f8aeaeedb77d58e4ebe0fdf4d4c
Namelss4 <jeronimocano22@gmail.com>
2023-04-01 12:32:53 -0500

    Update UnitTest1.cs
    Applied some basic tests after reviewing some general practices on unit testing.

--- TurnBasedCombatPrototype/UnitTest1.cs
+++ TurnBasedCombatPrototype/UnitTest1.cs
@@ -2,15 +2,52 @@ namespace TurnBasedCombatPrototype
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+        public int[] testArray;
+        //public int testStep;
+
+        //[SetUp]
+        //public void Setup()
+        //{
+        //    testArray = new int[] { 1, 2, 3 };
+        //}
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            //testArray = new int[] { 1, 2, 3 };
+            //FillTestArray(0);
+
+            Assert.Positive(testArray[1]);
+            Assert.NotNull(testArray);
         }
+
+        [Test]
+        public void Test2()
+        {
+            //testArray = new int[] { 1, 2, 3 };
+            //FillTestArray(2);
+
+            Assert.GreaterOrEqual(testArray.Length, 3);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            testArray = null;
+        }
+
+        //private void FillTestArray(int testStep)
+        //{
+        //    switch (testStep)
+        //    {
+        //        case 2:
+        //            testArray = new int[] { 1, 2, 3 };
+        //            break;
+
+        //        default:
+        //            testArray = new int[] { 1, 2, 3 };
+        //            break;
+        //    }
+        //}
     }
 }
